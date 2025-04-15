452a5a2604ef68502f0e442ec22e427ff59cf6e2
Raykeruse <raykerus.work@gmail.com>
2025-04-15 12:32:12 +0300

    Create test_google_logo.py

---
+++ tests/automation/test_google_logo.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def test_google_logo_is_displayed():
+    driver = webdriver.Chrome()
+    driver.get("https://www.google.com")
+
+    logo = driver.find_element(By.XPATH, "//img[@alt='Google']")
+    assert logo.is_displayed()
+
+    driver.quit()
